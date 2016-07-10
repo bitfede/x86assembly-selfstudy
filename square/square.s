9bcdd4354e4e89b17497492faf9fe3aa9b9697ab
Federico De Faveri <f.g.defaveri@gmail.com>
2016-07-10 08:11:05 -0400

    finished implementing and testing the square program

--- square/square.s
+++ square/square.s
@@ -2,7 +2,7 @@
 #
 # AUTHOR:      Federico De Faveri
 #
-# PURPOSE:     This program computes the square of a given value
+# PURPOSE:     This program simply computes the square of a given value
 #
 
 .code32
@@ -16,15 +16,16 @@
 
 _start:
   
-  movl        $2, %ebx                     #put the number 2 into %ebx  
+  movl        $3, %ebx                     #put the number to be squared into %ebx  
   push        %ebx                         #push the number to the stack. this is the function's parameter.
   call        square                       #call function square
   
-  ###TODO
+  addl        $4, %esp                     #returns the stack pointer register to pre-function call
+  movl        %eax, %ebx                   #moves the returned value in %eax to %ebx so we can print it in stdout with
+                                           # the "echo $?" command
   
-  
-  movl        $1 , %eax
-  int         $0x80
+  movl        $1 , %eax                    #puts 1 in %eax to indicate exit status
+  int         $0x80                        #sends interrupt to kernel! Goodbye!
 
   #  
   # FUNCTION:       this is the square function
@@ -39,14 +40,17 @@ _start:
   .type square, @function 
 square:
 
-  pushl       %ebp                          #save the old base pointer 
-  movl        %esp , %ebp                   #make the stack pointer the base pointer
-                                            #optionally here you can make room for local variables for extra storage
+  pushl       %ebp                          #save the old base pointer , freeze the current stack frame
+  movl        %esp , %ebp                   #make the stack pointer the base pointer, allowing me to access parameters easily
   
-  movl        8(%ebp) , %ebx                #load the first parameter to out %ebx register
-  imull       %ebx, %ebx, %eax              #multiply the number in %ebx by itself and store the result in %eax, which is where 
-                                            #return values usually go in functions
+  #                                         #optionally here you can make room for local variables for extra storage
   
-  movl        %ebp, %esp
-  popl        %ebp
-  ret
+  movl        8(%ebp) , %ebx                #load the first parameter to our %ebx register
+  movl        8(%ebp) , %eax                #load the first parameter to our %eax register
+  
+  imull       %ebx, %eax                    #multiply the number in %ebx by the number in %eax (by itself) and store the
+                                            #result in %eax, which is where return values usually go in functions
+  
+  movl        %ebp, %esp                    #restore the stack pointer register to the initial stack frame
+  popl        %ebp                          #restore old ebp
+  ret                                       #pops value on top of the stack (old return address) and puts it in %eip
