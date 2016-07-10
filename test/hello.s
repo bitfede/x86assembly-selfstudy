c359904b797e868d622e1e0467217128bccd9e7f
Federico De Faveri <f.g.defaveri@gmail.com>
2016-07-10 08:32:58 -0400

    polished and commented the test program

--- test/hello.s
+++ test/hello.s
@@ -1,3 +1,10 @@
+# LANGUAGE:    x86_64-pc-linux-gnu Assembly
+#
+# AUTHOR:      Federico De Faveri
+#
+# PURPOSE:     This program takes a number, adds 10 and exits
+#
+
 .code32
 
 .section .data
@@ -9,12 +16,13 @@
 
 _start:
 
-  movl $8 , %ebx
+  movl $8 , %ebx                #moving the value 8 into our %ebx register
 
-	addl $10 , %ebx
-	
+	addl $10 , %ebx               #adding 10 to the current value inside %ebx
+	                              # we will be able to retrieve this value by executing "echo $?" in the
+	                              # terminal after program execution
 
-  movl $1 , %eax
-  int $0x80
+  movl $1 , %eax                #giving status code 1, ready to exit
+  int $0x80                     #send interrupt signal to kernel
   
   
